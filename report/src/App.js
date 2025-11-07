1c4d59e278eb9027b1464c11880dc46085810a08
jiyaprajapati100 <jiyaprajapati100@gmail.com>
2025-11-07 05:42:42 +0000

    updated

--- report/src/App.js
+++ report/src/App.js
@@ -2,12 +2,13 @@ import React from 'react';
 import './App.css';
 //import LeadsBySourceList from './Leadbysourcelist';
 //import LeadsIntoDealsList from './Leadsintodeals';
-import DealsBySourceList from './Dealsbysourcelist';
+//import DealsBySourceList from './Dealsbysourcelist';
+import CommentsBySalesPerson from './Commentbysalesperson';
 
 function App() {
   return (
     <div className="App">
-      <DealsBySourceList />
+      <CommentsBySalesPerson />
     </div>
   );
 }
